152a5e252514c8f1689a40d86de1df8c94b3a72d
Fatih Oguz <oguzfatih39@gmail.com>
2023-08-25 14:37:43 +0300

    degisiklikler tamamlandi

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var swiftLabel: UILabel!
+    
+    
+    @IBOutlet weak var swiftImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ChangeButton(_ sender: Any) {
+        swiftImage.image=UIImage(named:"s2")
+    }
+    
 }
-
